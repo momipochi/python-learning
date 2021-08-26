5cc77bfad3fd9c195a67e83d8598ce5080630eb8
momipochi <k25650727@gmail.com>
2021-08-26 12:26:06 +0200

    first commit

---
+++ 1basic python/app.py
@@ -0,0 +1,16 @@
+
+
+def sayhi(sentence):
+    return f"{sentence} + some fucking shit yo";
+
+some_name = "fucks your name bro";
+age_of = 19;
+is_gay = False;
+
+print(f"whatis up {some_name} you are {age_of}, actually {is_gay}");
+
+
+print(isinstance(some_name, str));
+
+print(sayhi("fuck"));
+
